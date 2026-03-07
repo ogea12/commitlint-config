fa77765643123005a90263fc11392c5231e61ee7
Julien Benac <julien.benac@protonmail.com>
2026-03-07 19:33:32 +0100

    feat: add rules for `type-case` and `subject-case` to enforce lower-case formatting

--- index.ts
+++ index.ts
@@ -5,7 +5,9 @@ import { RuleConfigSeverity } from '@commitlint/types'
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'type-case': [RuleConfigSeverity.Error, 'always', 'lower-case'],
     'scope-case': [RuleConfigSeverity.Error, 'always', 'lower-case'],
+    'subject-case': [RuleConfigSeverity.Error, 'always', 'lower-case'],
   },
   helpUrl: 'https://github.com/ogea12/commitlint-config#readme',
 } satisfies UserConfig
